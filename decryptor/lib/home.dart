d68fe324ed6f589e1fe849a3dd5818b1cb7c3c33
kaefean1@gmail.com <kaan@Kaan-MacBook-Air.local>
2025-08-19 15:31:27 +0300

    decryptor is done

---
+++ decryptor/lib/home.dart
@@ -0,0 +1,34 @@
+import 'package:decryptor/dialog.dart';
+import 'package:decryptor/result.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class Home extends StatefulWidget {
+  const Home({super.key});
+
+  @override
+  State<Home> createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('Decryptor')),
+      body: Center(
+        child: FilledButton(
+          onPressed: () async {
+            final password = await showTextFieldDialog(context);
+            if (password == 'neon') {
+              Navigator.pushReplacement(
+                context,
+                MaterialPageRoute(builder: (context) => Result()),
+              );
+            }
+          },
+          child: Text('Decrypt'),
+        ),
+      ),
+    );
+  }
+}
